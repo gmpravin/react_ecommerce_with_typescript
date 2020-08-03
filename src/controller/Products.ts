5290787761b794b444b1da0bbdd595a6246e1095
gmpravin <40927922+gmpravin@users.noreply.github.com>
2020-08-03 22:28:39 +0530

    done

--- src/controller/Products.ts
+++ src/controller/Products.ts
@@ -7,9 +7,9 @@ import * as cloudinary from 'cloudinary';
 
 cloudinary.v2;
 cloudinary.v2.config({
-  cloud_name: 'reactcommerce',
-  api_key: '344174846447425',
-  api_secret: 'Dvo_ji1zPjoV9Ba7zHqAMopSPIM',
+  cloud_name: 'XXXXXXXXXXX',
+  api_key: 'XXXXXXXXXXX',
+  api_secret: 'XXXXXXXXXXX',
 });
 
 const Products = mongoose.model<IProductSchema>(
